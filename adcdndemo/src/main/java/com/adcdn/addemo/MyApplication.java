00363daf30223c91181230823d2a7130aa4dd20d
Lelouch123 <xnnzone@163.com>
2019-08-06 01:00:30 +0800

    优化sdk

--- adcdndemo/src/main/java/com/adcdn/addemo/MyApplication.java
+++ adcdndemo/src/main/java/com/adcdn/addemo/MyApplication.java
@@ -14,12 +14,14 @@ import com.yunxia.adsdk.tpadmobsdk.common.TPADMobSDK;
 
 public class MyApplication extends Application {
 
-    private String APP_ID = "30072";
+//    private String APP_ID = "30072";
+//    private String APP_ID = "1030008";
+    private String APP_ID = "1030007";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        // TODO: 2019/5/5 修改为自己的appId 测试id = 30072
+        // TODO: 2019/5/5 修改为自己的appId
         TPADMobSDK.instance().initSdk(getApplicationContext(), APP_ID);
 
     }
